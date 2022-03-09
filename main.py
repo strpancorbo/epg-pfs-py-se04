2b530d518a7148ee4d2692845c9fc6417706200a
strpancorbo <100705448+strpancorbo@users.noreply.github.com>
2022-03-09 14:26:29 +0100

    ejemplo.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Autor: Esther Pancorbo
 # version: 0.0.1
 # date: 09/03/2022
+# Auth: Ester Pancorbo
+# Version: 0.0.1
+# data: 02/03/2022
+# dupdateAT: --/--/----
 
+from Ejercicios1_18 import *
 
 if __name__ == '__main__':
-    print('hola mundo')
+    menu_principal()
+
+
